8e418b5cbae0bf11d42056cf1a0b5f9f5f5b92a0
tomystang <tomystang@tencent.com>
2019-10-30 16:02:38 +0800

    Comment feature that uses dark-grey list api.

--- matrix/matrix-android/matrix-io-canary/src/main/java/com/tencent/matrix/iocanary/detect/CloseGuardHooker.java
+++ matrix/matrix-android/matrix-io-canary/src/main/java/com/tencent/matrix/iocanary/detect/CloseGuardHooker.java
@@ -73,29 +73,30 @@ public final class CloseGuardHooker {
      */
     private boolean tryHook() {
         try {
-            Class<?> closeGuardCls = Class.forName("dalvik.system.CloseGuard");
-            Class<?> closeGuardReporterCls = Class.forName("dalvik.system.CloseGuard$Reporter");
-            Method methodGetReporter = closeGuardCls.getDeclaredMethod("getReporter");
-            Method methodSetReporter = closeGuardCls.getDeclaredMethod("setReporter", closeGuardReporterCls);
-            Method methodSetEnabled = closeGuardCls.getDeclaredMethod("setEnabled", boolean.class);
-
-            sOriginalReporter = methodGetReporter.invoke(null);
-
-            methodSetEnabled.invoke(null, true);
-
-            // open matrix close guard also
-            MatrixCloseGuard.setEnabled(true);
-
-            ClassLoader classLoader = closeGuardReporterCls.getClassLoader();
-            if (classLoader == null) {
-                return false;
-            }
-
-            methodSetReporter.invoke(null, Proxy.newProxyInstance(classLoader,
-                new Class<?>[]{closeGuardReporterCls},
-                new IOCloseLeakDetector(issueListener, sOriginalReporter)));
-
-            return true;
+            // Class<?> closeGuardCls = Class.forName("dalvik.system.CloseGuard");
+            // Class<?> closeGuardReporterCls = Class.forName("dalvik.system.CloseGuard$Reporter");
+            // Method methodGetReporter = closeGuardCls.getDeclaredMethod("getReporter");
+            // Method methodSetReporter = closeGuardCls.getDeclaredMethod("setReporter", closeGuardReporterCls);
+            // Method methodSetEnabled = closeGuardCls.getDeclaredMethod("setEnabled", boolean.class);
+            //
+            // sOriginalReporter = methodGetReporter.invoke(null);
+            //
+            // methodSetEnabled.invoke(null, true);
+            //
+            // // open matrix close guard also
+            // MatrixCloseGuard.setEnabled(true);
+            //
+            // ClassLoader classLoader = closeGuardReporterCls.getClassLoader();
+            // if (classLoader == null) {
+            //     return false;
+            // }
+            //
+            // methodSetReporter.invoke(null, Proxy.newProxyInstance(classLoader,
+            //     new Class<?>[]{closeGuardReporterCls},
+            //     new IOCloseLeakDetector(issueListener, sOriginalReporter)));
+            //
+            // return true;
+            throw new UnsupportedOperationException("unsupport now.");
         } catch (Throwable e) {
             MatrixLog.e(TAG, "tryHook exp=%s", e);
         }
@@ -105,18 +106,19 @@ public final class CloseGuardHooker {
 
     private boolean tryUnHook() {
         try {
-            Class<?> closeGuardCls = Class.forName("dalvik.system.CloseGuard");
-            Class<?> closeGuardReporterCls = Class.forName("dalvik.system.CloseGuard$Reporter");
-            Method methodSetReporter = closeGuardCls.getDeclaredMethod("setReporter", closeGuardReporterCls);
-            Method methodSetEnabled = closeGuardCls.getDeclaredMethod("setEnabled", boolean.class);
+            // Class<?> closeGuardCls = Class.forName("dalvik.system.CloseGuard");
+            // Class<?> closeGuardReporterCls = Class.forName("dalvik.system.CloseGuard$Reporter");
+            // Method methodSetReporter = closeGuardCls.getDeclaredMethod("setReporter", closeGuardReporterCls);
+            // Method methodSetEnabled = closeGuardCls.getDeclaredMethod("setEnabled", boolean.class);
+            //
+            // methodSetReporter.invoke(null, sOriginalReporter);
+            //
+            // methodSetEnabled.invoke(null, false);
+            // // close matrix close guard also
+            // MatrixCloseGuard.setEnabled(false);
 
-            methodSetReporter.invoke(null, sOriginalReporter);
-
-            methodSetEnabled.invoke(null, false);
-            // close matrix close guard also
-            MatrixCloseGuard.setEnabled(false);
-
-            return true;
+            // return true;
+            throw new UnsupportedOperationException("unsupport now.");
         } catch (Throwable e) {
             MatrixLog.e(TAG, "tryHook exp=%s", e);
         }
